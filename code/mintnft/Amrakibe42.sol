0b463d951202973959c2bc7f81becec1e8ef7668
amine rakibe <aminerakibe7@gmail.com>
2025-07-18 15:57:18 +0100

    update generate svg

--- code/mintnft/Amrakibe42.sol
+++ code/mintnft/Amrakibe42.sol
@@ -54,22 +54,22 @@ contract Amrakibe42 is ERC721URIStorage, Ownable {
         _setTokenURI(id, getTokenURI());
     }
 
-    function createSVG() internal view returns (string memory) {
+    
+function createSVG() internal view  returns (string memory) {
     string memory result = string(
         abi.encodePacked(
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">',
-                '<rect x="0.25" y="0.25" width="9.5" height="9.5" rx="1" ry="1" fill="#f0f4f8" stroke="#ccc" stroke-width="0.2"/>',
-                '<text x="5.05" y="5.05" font-family="Arial" font-size="2" fill="#aaa" text-anchor="middle" dominant-baseline="middle">',
-                '</text>',
-                '<text x="5" y="5" font-family="Arial" font-size="2" fill="#333" text-anchor="middle" dominant-baseline="middle">',
-                    metadata.visual,
-                '</text>',
-            '</svg>'
+            '<svg id="visual" viewBox="0 0 900 600" width="900" height="600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">'
+            '<rect x="0" y="0" width="900" height="600" fill="#FF0066"></rect>'
+            '<g transform="translate(406.32963196009666 322.44729477043194)">'
+            '<path d="M144.2 -146.2C192.2 -96.2 240.1 -48.1 237.5 -2.6C234.9 42.9 181.8 85.8 133.8 123.3C85.8 160.8 42.9 192.9 -4.8 197.7C-52.6 202.6 -105.1 180.1 -130.1 142.6C-155.1 105.1 -152.6 52.6 -145.1 7.4C-137.7 -37.7 -125.4 -75.4 -100.4 -125.4C-75.4 -175.4 -37.7 -237.7 5.2 -242.9C48.1 -248.1 96.2 -196.2 144.2 -146.2" fill="#BB004B"></path></g>'
+            '<text x="450" y="300" dominant-baseline="middle" text-anchor="middle" fill="white" font-size="120" font-weight="bold">',
+            metadata.visual,
+            '</text></svg>'
         )
     );
     return result;
 }
-    
+
     function getTokenURI() public view returns (string memory) {
         string memory encoded = Base64.encode(bytes(createSVG()));
         
